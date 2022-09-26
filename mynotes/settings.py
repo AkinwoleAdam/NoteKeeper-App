ae6a70d419aa8ada042ac30d51c8e3a63c0607a1
Akinwole Adam <akinwoleadam@gmail.com>
2022-09-26 19:22:51 +0100

    Deploying App on railway

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'mynotes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':'3LGgCquuB89mUaU8sOFO',
+        'HOST':'containers-us-west-50.railway.app',
+        'PORT':'6591',
     }
 }
 
