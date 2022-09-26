f9c761afd40bcfd06ae45c4725907a39d9cf7889
Akinwole Adam <akinwoleadam@gmail.com>
2022-09-26 19:41:39 +0100

    Final Deploymenton Railway App

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n_2dczb_(liko@leh^z9lsa(ua^0s^yk#si%0m9(+vn(!=jce%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['notekeeper-app.up.railway.app','127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https://notekeeper-app.up.railway.app']
 
 # Application definition
 
